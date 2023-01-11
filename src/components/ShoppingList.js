ed070511462709a0cb14ecf5231de6e8b017c4bd
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2023-01-12 00:19:25 +0300

    events

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const[filterterm, setFilterTerm] = useState("All");
+  function filterCategory(event){
+    setFilterTerm(event.target.value)
+  }
+  const displayItems = items.filter((item)=>{
+    if (filterterm === "All"){
+      return true
+    }
+    else{
+      return item.category === filterterm
+    }
+  })
+  console.log(filterterm)
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={filterCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
